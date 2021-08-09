81f22f7ad5f1d01b0657d58e2fc4a65c5abedfcc
matthewli1409 <matthewli1409@gmail.com>
2021-08-09 10:56:43 +0700

    fix

--- redis_keys.py
+++ redis_keys.py
@@ -1,8 +1,10 @@
 class RedisKey:
     """Redis keys"""
     EXCHANGE_WALLET = 'exchange-wallet'
-    MARGIN_WALLEt = 'margin-wallet'
+    MARGIN_WALLET = 'margin-wallet'
     ORDERBOOK_UPDATES = 'orderbook-updates'
     RESTING_ORDERS = 'resting-orders'
+    SYMBOLS_EXCLUDED = 'symbols-excluded'
+    SYMBOLS_INCLUDED = 'symbols-included'
     SYMBOLS_SUBBED = 'symbols-subbed'
     SYMBOLS_WALLET = 'symbols-wallet'
