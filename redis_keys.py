498d90b2dc36307a5f190a5c7e16ebf38c2840aa
matthewli1409 <matthewli1409@gmail.com>
2021-08-09 10:30:18 +0700

    fix

--- redis_keys.py
+++ redis_keys.py
@@ -1,7 +1,8 @@
-from enum import Enum
-
-
-class RedisKey(Enum):
+class RedisKey:
     """Redis keys"""
+    EXCHANGE_WALLET = 'exchange-wallet'
+    MARGIN_WALLEt = 'margin-wallet'
+    ORDERBOOK_UPDATES = 'orderbook-updates'
+    RESTING_ORDERS = 'resting-orders'
     SYMBOLS_SUBBED = 'symbols-subbed'
     SYMBOLS_WALLET = 'symbols-wallet'
