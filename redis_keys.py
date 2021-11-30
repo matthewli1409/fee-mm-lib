13fd2905092219dd99a2af1d2dfa4e3c9fb6ffd5
Man Kit Matthew Li <matthewli1409@gmail.com>
2021-11-30 16:29:09 +0700

    fix

--- redis_keys.py
+++ redis_keys.py
@@ -1,9 +1,9 @@
 class RedisKey:
+    ALGO_STALKER_DATA = 'stalker-data'
     EXCHANGE_WALLET = 'exchange-wallet'
     MARGIN_WALLET = 'margin-wallet'
     ORDERBOOK_UPDATES = 'orderbook-updates'
     RESTING_ORDERS = 'resting-orders'
-    STALKER_ORDERS = 'stalker-orders'
     SYMBOLS_EXCLUDED = 'symbols-excluded'
     SYMBOLS_INCLUDED = 'symbols-included'
     SYMBOLS_SUBBED_BFX = 'bfx-symbols-subbed'
