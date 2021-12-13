81c4b550da6bb9900474f1b3e55bc7c881454599
Man Kit Matthew Li <matthewli1409@gmail.com>
2021-12-13 12:28:13 +0700

    fix

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,8 @@
-import os
-import sys
-
-BFX_API_KEY = os.environ['BFX_MM_FEE_KEY_1']
-BFX_API_SECRET = os.environ['BFX_MM_FEE_SECRET_1']
-
-MONGO_CLIENT = os.environ['MONGODB_CLIENT']
-MONGO_DB = os.environ['BFX_FEE_DB']
-
-if len(sys.argv) > 1 and sys.argv[1] == 'localhost':
+class Settings:
+    BFX_KEY = data['BFX_KEY']
+    BFX_SECRET = data['BFX_SECRET']
+    MONGO_CLIENT = os.environ['MONGODB_CLIENT']
+    MONGO_DB = os.environ['BFX_FEE_DB']
+    PRIORITY_ORDERS = orders['data']
     REDIS_HOST = 'localhost'
-else:
-    REDIS_HOST = 'redis-cluster-ip-service'
-
-REDIS_PORT = 6379
+    REDIS_PORT = 6379
