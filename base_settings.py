6fc2b98be4330803ccbeb5c4b2967ff79473697e
Man Kit Matthew Li <matthewli1409@gmail.com>
2021-12-13 12:30:52 +0700

    base settings

--- base_settings.py
+++ base_settings.py
@@ -1,8 +1,5 @@
-class Settings:
-    BFX_KEY = data['BFX_KEY']
-    BFX_SECRET = data['BFX_SECRET']
+class BaseSettings:
     MONGO_CLIENT = os.environ['MONGODB_CLIENT']
     MONGO_DB = os.environ['BFX_FEE_DB']
-    PRIORITY_ORDERS = orders['data']
     REDIS_HOST = 'localhost'
     REDIS_PORT = 6379
