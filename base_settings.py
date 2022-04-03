51605d2131f8f257b218ce6e53acbd89d5c0743d
matthewli1409 <matthewli1409@gmail.com>
2022-04-03 11:55:15 +0700

    fix

--- base_settings.py
+++ base_settings.py
@@ -5,5 +5,10 @@ import sys
 class BaseSettings:
     MONGO_CLIENT = os.environ.get('MONGODB_CLIENT')
     MONGO_DB = os.environ.get('BFX_FEE_DB')
-    REDIS_HOST = 'localhost'
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'localhost':
+        REDIS_HOST = 'localhost'
+    else:
+        REDIS_HOST = 'redis-cluster-ip-service'
+        
     REDIS_PORT = 6379
