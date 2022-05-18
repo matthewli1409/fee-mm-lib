a66419adea34878848aac8427b425d502270bb8e
matthewli1409 <matthewli1409@gmail.com>
2022-05-18 09:08:00 +0700

    1.2.1

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='fee-lib',
-      version='1.1.4',
+      version='1.2.1',
       description='external api lib',
       url='',
       author='Matthew Li',
       author_email='matthewli1409@gmail.com',
       license='MIT',
-      install_requires=['dnspython==2.1.0', 'pymongo==3.11.0'],
+      install_requires=['dnspython==2.1.0', 'pymongo==3.11.0', 'python-dotenv==0.19.0', 'slackclient==2.5.0'],
       zip_safe=False)
